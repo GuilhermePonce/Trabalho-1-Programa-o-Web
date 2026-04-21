3111870da8258a550e04f92ff002f39e17498154
Guilherme Ponce <guilhermeponce47@gmail.com>
2026-04-21 15:49:53 -0300

    Adicionado build.sh
    Adicionado o arquivo build para subir no render

--- meeting_planner/settings.py
+++ meeting_planner/settings.py
@@ -76,6 +76,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'scheduler.User'
